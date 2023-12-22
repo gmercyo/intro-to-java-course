f1d1cfbe3191e21f7eb018f8a93fa54c489e724d
Mercy <gmercyoluwanusin@gmail.com>
2023-12-22 12:51:05 +0000

    Starting the algorithm exercise again

---
+++ exercises/algorithms/src/main/java/com/cbfacademy/sort/QuickSort.java
@@ -0,0 +1,14 @@
+package com.cbfacademy.sort;
+
+public class QuickSort implements ArraySorter {
+    
+
+    @Override
+    public void sort(int[] arr, int startIndex, int endIndex) {
+        this.arr = arr;
+        this.startIndex = startIndex;
+        this.endIndex = endIndex;
+    }
+
+    
+}
